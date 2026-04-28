1636e8d907cf25568e8b7c0bdcbd0410b1d415e5
BergaLou <157978333+BergaLou@users.noreply.github.com>
2026-04-28 18:45:02 +0200

    wavesprojects

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import data from "./data.json";
 import Hero from './components/Hero.jsx';
 import Skills from './components/Skills.jsx';
+import Projects from './components/Projects.jsx';
 import './App.css';
 
 export const App = () => {
   return <div>
     <Hero />
     <Skills />
+    <Projects />
   </div>
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,31 @@ import './Hero.css'
 
 function Hero() {
     return (
+    <div className="hero-main-wrapper">
         <section className="hero-container">
             
             <div className="hero-top-row">
             <img src="./assets/profil4.png" alt="My profile pic of me" className="hero-image"
             />
             <h2 className="hero-name">Clara Högberg</h2>
-            </div>
+    </div> 
+    </section>
+
+        <section className="HeroIntro">
+        <div className="custom-shape-divider-top-1777379739">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className="shape-fill"></path>
+            <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" className="shape-fill"></path>
+          <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" className="shape-fill"></path>
+         </svg>
+        </div>
 
             <div className="social-links">
-                <img src="./assets/Linkedin.png"
+                <img src="./assets/Linkedin2.png"
                 alt="Linkedin"
                 className="social-icon"
                 />
-                <img src="./assets/Github.png"
+                <img src="./assets/Github2.png"
                 alt="Github"
                 className="social-icon"
                 />
@@ -30,8 +41,16 @@ function Hero() {
 
             <p>I work with the whole chain, from design <i>(frontend)</i> to logic and databases <i>(backend)</i>. When I face a problem, I don’t give up until I find a good solution. I love learning new things and I want to work on projects where quality is important.</p>
             </div>
-
+            
+            <div className="custom-shape-divider-bottom-1777381986">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className="shape-fill"></path>
+            <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" className="shape-fill"></path>
+            <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" className="shape-fill"></path>
+            </svg>
+            </div>
         </section>
+    </div>
     )
 }
 
